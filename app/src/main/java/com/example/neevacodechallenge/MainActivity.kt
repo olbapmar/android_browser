b80579978043c22fbd9aa871295918077414f679
Pablo Pastor <olbap@outlook.com>
2021-12-01 20:15:55 +0000

    Initial version, setting up layouts and manifests. Logic part pending to be done

---
+++ app/src/main/java/com/example/neevacodechallenge/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.neevacodechallenge
+
+import android.content.Context
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
